717389bf9040aabfb00885cd8b9b576cb0b5b194
Pranav2612000 <pranav2000joglekar@gmail.com>
2020-11-29 18:43:00 +0530

    feat: Change about to 3rd person

--- src/Components/About.js
+++ src/Components/About.js
@@ -16,19 +16,19 @@ function About() {
             <p>Software Engineer | Full Stack Developer | Cyber Security Enthusiast </p>
 
 
-            <p> I am an undergraduate student, pursuing Bachelors in Information Technology at College of Engineering, Pune. I was introduced to the fabulous world of programming at the age of 13 through Scratch,(where I created my first game) and since then it has always fascinated me. Since then, for the past 9 years, I have learnt many new languages and technologies and worked on various projects. For me, writing code is like solving a puzzle - You have different pieces(the language syntax) and you need to put these pieces together correctly to solve the problem. And hey, I am a vim guy, in the age of IDEs, with only a Linux OS(No windows on dual boot!).
+            <p> Pranav Joglekar is an undergraduate student, pursuing Bachelors in Information Technology at College of Engineering, Pune. Pranav was introduced to the fabulous world of programming at the age of 13 through Scratch and it fascinated him. Since then, for the past 9 years, Pranav has learnt many new languages and technologies and worked on various projects. For him, writing code is like solving a puzzle - You have different pieces(the language syntax) and you need to put these pieces together correctly to solve the problem. And he is a vim guy, in the age of IDE.
             </p>
 
             <p>
-          The fields that interest and challenge me are - Algorithms, OS, Networks & Security ( I know that's very diverse!). I am currently exploring all these fields through various projects. I am taking multiple advanced courses online on these fields to know them better and chose my specialization.(No luck so far).
+          Pranav Joglekar finds Algorithms, OS, Networks & Security interesting. He is currently exploring all these fields through various projects. He is taking multiple advanced courses online on these fields to know them better and chose his specialization.(No luck so far).
             </p>
 
             <p>
-            I love to debate & discuss about computers and technical stuff. Drop me a PM, if you have any interesting topics for discussion or if you are interested in hiring me ( Yes, good news, I am currently unemployed).
+            Pranav Joglekar love to debate & discuss about computers and technical stuff. Drop him a PM, if you have any interesting topics for discussion or if you are interested in hiring him( Yes, good news, He is currently unemployed).
             </p>
 
             <p>
-            I like to read books and solve puzzles. . I am good at quickly learning and adapting to new technologies and programming languages and hence can easily fit into new teams. I am able to stay calm and make rational decisions under stress. For stress busting, I play sports. 
+            Pranav Joglekar likes to read books and solve puzzles. He is good at quickly learning and adapting to new technologies and programming languages and hence can easily fit into new teams. I am able to stay calm and make rational decisions under stress. For stress busting, He plays sports. 
             </p>
           </div>
 
