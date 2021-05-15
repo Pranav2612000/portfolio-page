1d184f52f43758202915efe4d9fdcb499bb52320
Pranav2612000 <pranav2000joglekar@gmail.com>
2021-05-15 13:37:10 +0530

    refactor: remove unrequired imports

--- src/pages/projects.jsx
+++ src/pages/projects.jsx
@@ -3,14 +3,7 @@ import ReactDOM from 'react-dom';
 import '../index.css';
 import '../App.css';
 import Layout from '../Components/Layout.jsx';
-import Hero from '../Components/Hero';
-import About from '../Components/About';
 import Projects from '../Components/Projects';
-import Contact from '../Components/Contact';
-import Footer from '../Components/Footer';
-import Skills from '../Components/Skills';
-import Resume from '../Components/Resume';
-import Life from '../Components/Life';
 
 const ProjectsPage = () => {
   return (
