d6064d7656381656febc63ea64e253aa56f537c0
Pranav2612000 <pranav2000joglekar@gmail.com>
2021-05-16 01:59:23 +0530

    feat: update the meta tags to use absolute paths

--- src/Components/Layout.jsx
+++ src/Components/Layout.jsx
@@ -44,21 +44,31 @@ class Layout extends React.Component {
             <Helmet>
                 <title>Portfolio | Pranav Joglekar</title>
                 <link rel="icon" href={favicon}/>
-                <link rel="canonical" href="https://pranavjoglekarcodes.web.app" />
+                <link rel="canonical" href="https://pranavjoglekarcodes.web.app"/>
                 <meta name="description" content="Pranav Joglekar's Portfolio Page. See a list of Pranav's Projects or
                 read his blogs"/>
                 <meta name="viewport" content="width=device-width, initial-scale=1"/>
+                <meta name="keywords" content="Pranav Joglekar Portfolio Blog"/>
+                <meta name="author" content="Pranav Joglekar"/>
 
                 <meta property="og:title" content="Pranav Joglekar | Portfolio"/>
                 <meta property="og:description" content="Pranav Joglekar's portfolio and blog website. Blogs written by Pranav hosted here.
                 Contains Pranav's work and projects. Visit this if page if you want to hire a rockstar developer"/>
-                <meta property="og:image" content={mugShot}/>
+                <meta property="og:image" content={"https://pranavjoglekarcodes.web.app/" + mugShot}/>
+                <meta property="og:image:width" content="600" />
+                <meta property="og:image:height" content="600" />
                 <meta property="og:url" content="https://pranavjoglekarcodes.web.app"/>
+                <meta property="og:locale" content="en_GB" />
+                <meta property="og:type" content="website" />
 
-                <meta name="twitter:title" content="Pranav Joglekar | Portfolio"/>
-                <meta name="twitter:description" content="Pranav Joglekar's portfolio and blog"/>
-                <meta name="twitter:image" content={mugShot}/>
                 <meta name="twitter:card" content="summary_large_image"/>
+                <meta name="twitter:description" content="Pranav Joglekar's portfolio and blog"/>
+                <meta name="twitter:title" content="Pranav Joglekar | Portfolio"/>
+                <meta name="twitter:image" content={"https://pranavjoglekarcodes.web.app/" + mugShot}/>
+
+                <meta name="twitter:site" content="@joglekar_pranav" />
+                <meta name="twitter:creator" content="@joglekar_pranav" />
+
 
                 <meta property="og:site_name" content="Pranav Joglekar | Portfolio"/>
                 <meta name="twitter:image:alt" content="Pranav Joglekar"/>
