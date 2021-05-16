427f0efe5905be3e16212f70e341574f18856d0d
Pranav2612000 <pranav2000joglekar@gmail.com>
2021-05-16 10:38:50 +0530

    feat: add google analytics to the site

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,13 @@
 module.exports = {
   plugins: [
     {
+      resolve: "gatsby-plugin-google-tagmanager",
+      options: {
+        id: "GTM-KHW3X7J",
+        includeInDevelopment: false,
+      },
+    },
+    {
       resolve: `gatsby-plugin-google-fonts`,
       options: {
         fonts: [
@@ -12,7 +19,9 @@ module.exports = {
     },
     `gatsby-plugin-image`,
     `gatsby-plugin-sharp`,
-    `gatsby-transformer-sharp`
+    `gatsby-transformer-sharp`,
+    'gatsby-plugin-react-helmet',
+    'gatsby-plugin-offline',
   ],
   flags: {
     THE_FLAG: false,
