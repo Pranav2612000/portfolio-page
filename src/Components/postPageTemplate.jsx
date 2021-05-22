72f6a1863317a6e2eda8e7ee6daa2be79c59a6e6
Pranav2612000 <pranav2000joglekar@gmail.com>
2021-05-22 14:42:00 +0530

    feat: move postPage styles to a different file

--- src/Components/postPageTemplate.jsx
+++ src/Components/postPageTemplate.jsx
@@ -4,6 +4,7 @@ import React from 'react'
 import Layout from '../Components/Layout.jsx';
 import Helmet from 'react-helmet'
 import { GatsbyImage, getImage } from "gatsby-plugin-image"
+import "../styles/postPageTemplate.css";
 
 export const query = graphql
     `
