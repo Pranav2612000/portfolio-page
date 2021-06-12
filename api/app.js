800df9d89a94dae546e4c7c49140c0bfe4942a05
Pranav2612000 <pranav2000joglekar@gmail.com>
2021-06-12 18:16:28 +0530

    feat: return the list of blogs

--- api/app.js
+++ api/app.js
@@ -2,10 +2,47 @@ require('dotenv').config();
 const fastify = require('fastify');
 const PORT = process.env.PORT;
 
+const MongoClient = require('mongodb').MongoClient;
+const uri = process.env.MONGO_URI;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
 function init() {
+
   const app = fastify({logger: true});
   app.get('/.netlify/functions/server', function (request, reply) {
     reply.send({ hello: 'world' });
+  });
+
+  app.get('/.netlify/functions/server/getBlogs', async function (request, reply) {
+    try {
+
+      const MongoClient = require('mongodb').MongoClient;
+      const uri = process.env.MONGO_URI;
+      const client = new MongoClient(uri, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+      });
+      let db;
+      console.log("connecting to MongoDB");
+      await client.connect();
+      db = client.db("personalSite");
+
+      const blogsCollection = db.collection('blogs');
+
+      const blogsCursor = await blogsCollection.find();
+
+      const blogs = [];
+      await blogsCursor.forEach((x) => {
+        blogs.push(x);
+      });
+      reply.send({success: blogs});
+    } catch(err) {
+      console.log(err);
+      reply.send({err: err});
+    }
   });
   return app;
 }
