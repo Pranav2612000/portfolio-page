dce58548d3e5e4145906e5082888ecf3b1eb26a5
Pranav2612000 <pranav2000joglekar@gmail.com>
2021-06-12 18:45:23 +0530

    feat: do not make a new db connection if the previous was cached

--- api/app.js
+++ api/app.js
@@ -12,22 +12,28 @@ const client = new MongoClient(uri, {
 function init() {
 
   const app = fastify({logger: true});
+
+  const MongoClient = require('mongodb').MongoClient;
+  const uri = process.env.MONGO_URI;
+  let client = new MongoClient(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  });
+  let db;
+
   app.get('/.netlify/functions/server', function (request, reply) {
     reply.send({ hello: 'world' });
   });
 
   app.get('/.netlify/functions/server/getBlogs', async function (request, reply) {
-    try {
 
-      const MongoClient = require('mongodb').MongoClient;
-      const uri = process.env.MONGO_URI;
-      const client = new MongoClient(uri, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true
-      });
-      let db;
+    if(!db || !db.serverConfig.isConnected()) {
       console.log("connecting to MongoDB");
       await client.connect();
+    }
+    console.log("using cached mongo connection");
+
+    try {
       db = client.db("personalSite");
 
       const blogsCollection = db.collection('blogs');
