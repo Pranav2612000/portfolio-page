3c838d5050adad1d4f543d5043a5ebe227d986d3
Pranav2612000 <pranav2000joglekar@gmail.com>
2021-06-12 16:34:09 +0530

    test: check if /server works

--- api/app.js
+++ api/app.js
@@ -4,6 +4,9 @@ const PORT = process.env.PORT;
 
 function init() {
   const app = fastify({logger: true});
+  app.get('/server', function (request, reply) {
+    reply.send({ hello: 'world' });
+  });
   app.get('/', function (request, reply) {
     reply.send({ hello: 'world' });
   });
@@ -12,6 +15,7 @@ function init() {
 
 
 if (require.main === module) {
+  console.log("here");
   const fastify = init();
   fastify.listen(PORT, '0.0.0.0', function (err, address) {
     if (err) {
@@ -21,5 +25,7 @@ if (require.main === module) {
     fastify.log.info(`server listening on ${address}`);
   });
 } else {
+  console.log("here too");
+    fastify.log.info(`server listening on h`);
   module.exports = init;
 }
