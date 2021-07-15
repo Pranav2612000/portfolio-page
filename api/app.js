471135e959b20f11d962b3967ba550c43493df79
Pranav2612000 <pranav2000joglekar@gmail.com>
2021-07-16 01:27:29 +0530

    improv: use fastify-cors for cors setup

--- api/app.js
+++ api/app.js
@@ -6,10 +6,9 @@ const MongoClient = require('mongodb').MongoClient;
 const uri = process.env.MONGO_URI;
 
 const registerPlugins = async (app) => {
-  app.register(require('fastify-express'))
-  .then((app) => {
-    app.use(require('cors')())
-  })
+  app.register(require('fastify-cors'), { 
+    origin: "*"
+  });
 }
 
 function init() {
