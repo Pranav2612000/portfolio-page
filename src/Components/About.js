32b1c3f60c3681aa1efc49ff42b0a5fad8fd999a
Pranav2612000 <pranav2000joglekar@gmail.com>
2024-12-14 20:05:54 +0530

    content: update About Me section

--- src/Components/About.js
+++ src/Components/About.js
@@ -19,22 +19,24 @@ function About() {
           </div>
 
           <div className="about-pranav-summary">
-            <p>Software Engineer | Full Stack Developer | Cyber Security Enthusiast </p>
+            <p>Engineer who loves to solve tough problems</p>
 
 
-            <p>Pranav is currently working as a software Developer at Avaya, and as the Global DevOps Manager a Bridges For Enterprise. Pranav was introduced to the fabulous world of programming at the age of 13 through Scratch and it fascinated him. Since then, for the past 9 years, Pranav has learnt many new languages and technologies and worked on various projects. For him, writing code is like solving a puzzle - You have different pieces(the language syntax) and you need to put these pieces together correctly to solve the problem. And he is a vim guy, in the age of IDE.
+            <p>
+            I am a Full Stack expert building an API first world at <a href="https://postman.com/">Postman</a>. I was introduced to the world of programming at the age of 13 through Scratch, (using which I created my first game) and I was enamoured by it. Since then, for the past 10 years, I've been tinkering with technology to craft beautiful websites, build awesome products and solve problems. I'm a hacker at heart - not building something makes me restless.
             </p>
 
             <p>
-          Pranav Joglekar finds Algorithms, OS, Networks & Security interesting. He is currently exploring all these fields through various projects. He is taking multiple advanced courses online on these fields to know them better and chose his specialization.(No luck so far).
+            For me, writing code is like solving a puzzle - You have different pieces(the language syntax) and you need to put these pieces together appropriately to solve a problem. And hey, I am a vim guy, in the age of IDEs, who loves to contribute to Open Source. I'm a Javascript Wizard, tech polyglot, DevOps aficionado and someone who gets the job done - irrespective of the technology. This has been made possible by my strong fundamentals in Algorithms, OS, Networks & Security.
             </p>
 
             <p>
-            Pranav Joglekar love to debate & discuss about computers and technical stuff. Drop him a PM, if you have any interesting topics for discussion or if you are interested in hiring him( Yes, good news, He is currently unemployed).
+            I love to debate about computers, technology & puzzles. I'm an avid reader - Books, Blogs, Newsletters & Tweets - I read everything. When not reading, I write tech blogs here. 
+            Drop me a PM, if you have any interesting topics for discussion.
             </p>
 
             <p>
-            Pranav Joglekar likes to read books and solve puzzles. He is good at quickly learning and adapting to new technologies and programming languages and hence can easily fit into new teams. I am able to stay calm and make rational decisions under stress. For stress busting, He plays sports. 
+            My curiosity makes me a quick learner which has helped me adapt to new technologies and programming languages. I am able to stay calm and make rational decisions under stress. For stress busting, I run marathons and play basketball.
             </p>
           </div>
 
